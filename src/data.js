369fe1956a81d965339f6b36e2aba4b6b2e7e276
AltafShk <altaf_and18@hotmail.com>
2024-04-21 02:52:12 +0200

    edit skills

--- src/data.js
+++ src/data.js
@@ -40,50 +40,127 @@ export const moreInfo =
 export const skillData = [
   {
     id: 1,
-    skill: <Icon icon="mdi:language-html5" className="display-4" />,
+    skill: (
+      <Icon
+        icon="vscode-icons:file-type-html"
+        className="display-4 text-primary"
+      />
+    ),
     name: "HTML5",
   },
   {
     id: 2,
-    skill: <Icon icon="ion:logo-css3" className="display-4" />,
+    skill: (
+      <Icon
+        icon="vscode-icons:file-type-css"
+        className="display-4 text-primary"
+      />
+    ),
     name: "CSS3",
   },
   {
     id: 3,
-    skill: <Icon icon="fa6-brands:js" className="display-4" />,
+    skill: (
+      <Icon
+        icon="vscode-icons:file-type-js"
+        className="display-4 text-primary"
+      />
+    ),
     name: "JavaScript",
   },
   {
     id: 4,
-    skill: <Icon icon="ri:bootstrap-fill" className="display-4" />,
-    name: "BootStrap",
-  },
-  {
-    id: 5,
-    skill: <Icon icon="mdi:react" className="display-4" />,
+    skill: (
+      <Icon
+        icon="vscode-icons:file-type-reactjs"
+        className="display-4 text-primary"
+      />
+    ),
     name: "React",
   },
   {
-    id: 6,
-    skill: <Icon icon="file-icons:styledcomponents" className="display-4" />,
-    name: "Styled Components",
+    id: 5,
+    skill: (
+      <Icon
+        icon="vscode-icons:file-type-angular"
+        className="display-4 text-primary"
+      />
+    ),
+    name: "Angular",
   },
   {
     id: 7,
-    skill: <Icon icon="akar-icons:redux-fill" className="display-4" />,
-    name: "Redux",
+    skill: (
+      <Icon
+        icon="vscode-icons:file-type-svelte"
+        className="display-4 text-primary"
+      />
+    ),
+    name: "Svelte",
   },
   {
     id: 8,
-    skill: <Icon icon="bi:git" className="display-4" />,
-    name: "Git",
+    skill: (
+      <Icon
+        icon="vscode-icons:file-type-node"
+        className="display-4 text-primary"
+      />
+    ),
+    name: "Node.js",
   },
   {
     id: 9,
-    skill: <Icon icon="fa6-brands:square-github" className="display-4" />,
-    name: "GitHub",
+    skill: <Icon icon="logos:firebase" className="display-4 text-primary" />,
+    name: "Firebase",
+  },
+  {
+    id: 10,
+    skill: (
+      <Icon
+        icon="vscode-icons:file-type-mongo"
+        className="display-4 text-primary"
+      />
+    ),
+    name: "MongoDB",
+  },
+  {
+    id: 11,
+    skill: (
+      <Icon
+        icon="vscode-icons:file-type-typescript"
+        className="display-4 text-primary"
+      />
+    ),
+    name: "TypeScript",
+  },
+  {
+    id: 12,
+    skill: (
+      <Icon
+        icon="vscode-icons:file-type-aws"
+        className="display-4 text-primary"
+      />
+    ),
+    name: "AWS",
+  },
+  {
+    id: 13,
+    skill: (
+      <Icon
+        icon="vscode-icons:file-type-python"
+        className="display-4 text-primary"
+      />
+    ),
+    name: "Python",
   },
 ];
+
+
+
+
+
+
+
 
 // Resume link (string - "https://YourResumeUrl") - I am using CloudFront to share my resume (https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/Introduction.html)
 export const resume = null;
